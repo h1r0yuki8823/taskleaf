bf61bfbc8ab4d136cac7309ab3ce72a19d025239
h1r0yuki8823 <hiroyukiandswift@gmail.com>
2018-11-03 23:01:45 +0900

    基本機能実装

---
+++ taskleaf/test/controllers/taks_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class TaksControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get taks_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get taks_show_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get taks_new_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get taks_edit_url
+    assert_response :success
+  end
+
+end
