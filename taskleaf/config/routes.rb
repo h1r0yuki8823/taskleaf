5292c6c39df3b0e07c233aefebbbef310b22a947
h1r0yuki8823 <hiroyukiandswift@gmail.com>
2018-11-07 08:20:27 +0900

    確認画面と機能の追加

--- taskleaf/config/routes.rb
+++ taskleaf/config/routes.rb
@@ -2,14 +2,14 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+
   namespace :admin do
     resources :users
   end
-  match '/login', to: 'sessions#create', via: [:post, :patch, :put]
+
   root to: 'tasks#index'
+
   resources :tasks do 
-    collection do 
-      get 'export'
-    end
+    post :confirm, action: :confirm_new, on: :new
   end
 end
